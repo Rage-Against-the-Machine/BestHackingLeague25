b06c124e357286f2ca104172a2aaee98fa195f37
Wojciech Bakun <wojciechzsee@gmail.com>
2025-11-29 23:57:56 +0100

    added savings widget

--- mobile_app/lib/features/profile/view/user_page.dart
+++ mobile_app/lib/features/profile/view/user_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mobile_app/core/theme/app_colors.dart';
 import 'package:mobile_app/core/theme/app_typography.dart';
 import 'package:mobile_app/features/profile/viewModel/user_viewModel.dart';
-import 'package:qr_flutter/qr_flutter.dart'; // Importujemy bibliotekę QR
+import 'package:qr_flutter/qr_flutter.dart';
 
 class UserPage extends StatelessWidget {
   final UserViewModel userViewModel;
@@ -19,23 +19,25 @@ class UserPage extends StatelessWidget {
             children: [
               Padding(
                 padding: const EdgeInsets.only(bottom: 32.0),
-                child: Text('Mój Profil', style: AppTypography.headline),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.person_outline_outlined,
+                      size: 32,
+                      color: AppColors.textPrimary,
+                    ),
+                    Text(
+                      userViewModel.user.id,
+                      style: AppTypography.productLabel,
+                    ),
+                  ],
+                ),
               ),
 
               Container(
                 padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(12),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.1),
-                      spreadRadius: 2,
-                      blurRadius: 8,
-                      offset: const Offset(0, 4),
-                    ),
-                  ],
-                ),
+                decoration: BoxDecoration(color: Colors.white),
                 child: QrImageView(
                   data: userViewModel.qrData,
                   version: QrVersions.auto,
@@ -46,7 +48,7 @@ class UserPage extends StatelessWidget {
                     dataModuleShape: QrDataModuleShape.square,
                   ),
                   eyeStyle: QrEyeStyle(
-                    color: AppColors.accent,
+                    color: AppColors.textPrimary,
                     eyeShape: QrEyeShape.square,
                   ),
                 ),
@@ -59,11 +61,56 @@ class UserPage extends StatelessWidget {
                 ),
                 textAlign: TextAlign.center,
               ),
-
-              // Możesz dodać tutaj inne elementy, np. przyciski, linki
+              const SizedBox(height: 16),
+              SavedMoneyWidget(
+                totalSavings: userViewModel.totalSavings.toDouble(),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class SavedMoneyWidget extends StatelessWidget {
+  final double totalSavings;
+
+  const SavedMoneyWidget({required this.totalSavings, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Formatowanie kwoty na string z dwoma miejscami po przecinku i walutą
+    final String formattedSavings = '${totalSavings.toStringAsFixed(2)} PLN';
+
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15.0),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: AppColors.discountBadgeBackground,
+          width: 1.5,
+        ),
+      ),
+      child: Column(
+        children: [
+          Text(
+            'Łącznie zaoszczędzono:'.toUpperCase(),
+            style: AppTypography.caption.copyWith(
+              fontSize: 14,
+              color: AppColors.textSecondary,
+            ),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            formattedSavings,
+            style: AppTypography.price.copyWith(
+              fontSize: 32.0, // Duża czcionka, by podkreślić wartość
+              fontWeight: FontWeight.w900,
+              color: AppColors.discountBadgeBackground,
+            ),
+          ),
+        ],
       ),
     );
   }
--- mobile_app/lib/features/profile/viewModel/user_viewmodel.dart
+++ mobile_app/lib/features/profile/viewModel/user_viewmodel.dart
@@ -1,9 +1,13 @@
 import 'package:mobile_app/features/profile/model/user.dart';
 
 class UserViewModel {
-  final User qrCode = User(id: '1234567890');
+  final User user = User(id: '1234567890', totalSavings: 100);
 
   String get qrData {
-    return qrCode.id;
+    return user.id;
+  }
+
+  int get totalSavings {
+    return user.totalSavings ?? 0;
   }
 }
