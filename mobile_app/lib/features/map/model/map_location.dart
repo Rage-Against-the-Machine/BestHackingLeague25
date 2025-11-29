cfcf460ca1ee675acbd52660fbb83e967f9e21ec
Wojciech Bakun <wojciechzsee@gmail.com>
2025-11-29 22:57:46 +0100

    added map page

---
+++ mobile_app/lib/features/map/model/map_location.dart
@@ -0,0 +1,11 @@
+class MapLocation {
+  final double lat;
+  final double lng;
+  final String label;
+
+  const MapLocation({
+    required this.lat,
+    required this.lng,
+    required this.label,
+  });
+}
--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app/core/theme/app_colors.dart';
 import 'package:mobile_app/core/theme/app_theme.dart';
+import 'package:mobile_app/features/map/view/map_page.dart';
 import 'package:mobile_app/features/products/view/products_page.dart';
 
 void main() {
@@ -17,9 +18,21 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int currentPageIndex = 0;
 
+  // Lista widżetów/stron, które będą wyświetlane
+  final List<Widget> _pages = [
+    // 0. Strona Produktów
+    ProductsPage(),
+
+    MapPage(),
+
+    // 2. Strona Profilu (Placeholder)
+    Center(child: Text('Strona Profilu', style: TextStyle(fontSize: 24))),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Pamiętaj, aby upewnić się, że AppTheme.lightTheme.colorScheme jest poprawnie zdefiniowany
       theme: ThemeData(colorScheme: AppTheme.lightTheme.colorScheme),
       title: 'GAZETKA',
       home: Scaffold(
@@ -41,7 +54,8 @@ class _MyAppState extends State<MyApp> {
             NavigationDestination(icon: Icon(Icons.person), label: 'Profil'),
           ],
         ),
-        body: ProductsPage(),
+
+        body: _pages[currentPageIndex],
       ),
     );
   }
