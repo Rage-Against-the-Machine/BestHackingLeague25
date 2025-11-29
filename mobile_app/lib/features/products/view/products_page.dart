389a1b64527792c8d8e7ec35fba8d853535a678d
Wojciech Bakun <wojciechzsee@gmail.com>
2025-11-29 22:03:45 +0100

    added nav bar

--- mobile_app/lib/features/products/view/products_page.dart
+++ mobile_app/lib/features/products/view/products_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app/core/theme/app_colors.dart';
+import 'package:mobile_app/core/theme/app_typography.dart';
 import 'package:mobile_app/features/products/view/product_card.dart';
 
 class ProductsPage extends StatelessWidget {
@@ -9,11 +10,25 @@ class ProductsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: ListView.builder(
-        itemCount: 20,
-        itemBuilder: (context, index) {
-          return ProductCard();
-        },
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text('GAZETKA', style: AppTypography.headline),
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return ProductCard();
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
