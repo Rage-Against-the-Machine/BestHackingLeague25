84decfba358f6b155fdf568e5858e33502d31bf9
Wojciech Bakun <wojciechzsee@gmail.com>
2025-11-29 22:03:45 +0100

    added nav bar

--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/core/theme/app_colors.dart';
 import 'package:mobile_app/core/theme/app_theme.dart';
 import 'package:mobile_app/features/products/view/products_page.dart';
 
@@ -6,15 +7,42 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(colorScheme: AppTheme.lightTheme.colorScheme),
-      home: ProductsPage(),
+      title: 'GAZETKA',
+      home: Scaffold(
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          selectedIndex: currentPageIndex,
+          indicatorColor: AppColors.accent,
+
+          destinations: const [
+            NavigationDestination(
+              icon: Icon(Icons.discount),
+              label: 'Produkty',
+            ),
+            NavigationDestination(icon: Icon(Icons.map), label: 'Mapa'),
+            NavigationDestination(icon: Icon(Icons.person), label: 'Profil'),
+          ],
+        ),
+        body: ProductsPage(),
+      ),
     );
   }
 }
